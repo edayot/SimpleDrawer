3a18984374eafb48d14e244f831896738cca1420
Erwan DAYOT <pro.e.dayot@gmail.com>
2021-12-22 11:36:45 +0100

    structure change with mklink

---
+++ mklink.bat
@@ -0,0 +1,3 @@
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\New World (22)\datapacks\SimpleDrawer" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer RessourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer RessourcePack"
+PAUSE
