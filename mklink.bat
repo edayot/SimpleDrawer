3b440bec3c7e91bb51beacf6c667a06150df093e
edayot <pro.e.dayot@gmail.com>
2022-04-11 22:06:34 +0200

    new craft with smithed

--- mklink.bat
+++ mklink.bat
@@ -1,4 +1,5 @@
-
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack" "D:\Documents\Python VS\SimpleDrawer\SimpleDrawer DataPack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer ResourcePack" "D:\Documents\Python VS\SimpleDrawer\SimpleDrawer ResourcePack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer ResourcePack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer ResourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer ResourcePack"
 PAUSE
