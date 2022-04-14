60f4b4c6d54f0a4ae4bae2a4ed86ee5192c8e9fc
edayot <pro.e.dayot@gmail.com>
2022-04-14 11:28:45 +0200

    auto update book in crafting table +banner update

---
+++ SimpleDrawer DataPack/data/simpledrawer/functions/custom_crafts/shapeless.mcfunction
@@ -0,0 +1,1 @@
+execute store result score @s smithed.data if entity @s[scores={smithed.data=0}] if score count smithed.data matches 1 if data storage smithed.crafter:main root.temp{shapeless_crafting_input:[{Count:1b,tag:{ctc:{from:"airdox_:simpledrawer",id:"guide",traits:{items:1b}}}}]} run loot replace block ~ ~ ~ container.16 loot simpledrawer:items/guide
--- mklink.bat
+++ mklink.bat
@@ -1,8 +1,8 @@
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack"
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer ResourcePack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\resourcepacks\SimpleDrawer ResourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer ResourcePack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimpleDrawer ResourcePack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimpleDrawer ResourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer ResourcePack"
 PAUSE
 
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\datapacks\SimpleDrawer DataPack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.1\.minecraft\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\datapacks\SimpleDrawer DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
