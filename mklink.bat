3c528ab9871f554f45476a05a8cb617d3f45087d
edayot <pro.e.dayot@gmail.com>
2022-06-08 22:00:56 +0200

    update to 1.19

--- mklink.bat
+++ mklink.bat
@@ -1,5 +1,5 @@
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack"
-rd "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimpleDrawer ResourcePack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\saves\Datapack island\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
-mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.18.2\.minecraft\resourcepacks\SimpleDrawer ResourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer ResourcePack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\1.19\saves\Datapack island\datapacks\SimpleDrawer DataPack"
+rd "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\SimpleDrawer ResourcePack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\1.19\saves\Datapack island\datapacks\SimpleDrawer DataPack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer DataPack"
+mklink /d "C:\Users\erwan\AppData\Roaming\.instance\resourcepacks\SimpleDrawer ResourcePack" "C:\Users\erwan\Documents\Dev\SimpleDrawer\SimpleDrawer ResourcePack"
 PAUSE
