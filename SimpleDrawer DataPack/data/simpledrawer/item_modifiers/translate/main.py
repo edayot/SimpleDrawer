df981caa96f5b93212b315a8eb1f22d54b98c4d9
edayot <pro.e.dayot@gmail.com>
2022-06-30 20:00:46 +0200

    1.19 items ne sont pas dans le Lore

--- SimpleDrawer DataPack/data/simpledrawer/item_modifiers/translate/main.py
+++ SimpleDrawer DataPack/data/simpledrawer/item_modifiers/translate/main.py
@@ -67,7 +67,7 @@ def generate(id,translate):
 
 def get_ids():
     #Get all items ids
-    with open("all.json","r") as f:
+    with open("all.jsonD","r") as f:
         data=json.load(f)
     return data["values"]
 
@@ -75,12 +75,14 @@ def get_translate_from_id(id):
     #Get translate from id
     translate=id
     translate=translate.replace(":",".")
-    with open("en_us.json","r") as f:
+    with open("en_us.jsonD","r") as f:
         data=json.load(f)
         if "block."+translate in data:
             return "block."+translate
         elif "item."+translate in data:
             return "item."+translate
+        else:
+            return None
     
 
 def main():
@@ -92,5 +94,13 @@ def main():
         if translate is not None:
             item_modifier=generate(id,translate)
             I.append(item_modifier)
+        else:
+            print("No translate for "+id)
+            #delete id from all.jsonD
+            with open("all.jsonD","r") as f:
+                data=json.load(f)
+                data["values"].remove(id)
+            with open("all.jsonD","w") as f:
+                json.dump(data,f,indent=4)
     with open("test_translate.json","w") as f:
         json.dump(I,f,indent=4)
