33b479b13b4688b13900cdab1550c04c343c3644
edayot <pro.e.dayot@gmail.com>
2023-01-28 15:07:03 +0100

    fix: new itemio requirement

--- beet_watch.bat
+++ beet_watch.bat
@@ -1,2 +1,2 @@
-beet l --minecraft "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft" --data-pack "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft/saves/Datapack island/datapacks" --resource-pack "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft/resourcepacks"
+beet l --minecraft "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft" --data-pack "C:\Users\erwan\Documents\Serveur 1.19.3\world\datapacks" --resource-pack "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\Fabulously Optimized\minecraft/resourcepacks"
 beet --project .\beet-dev.yaml w
