34369ace31282b60b28ada254562e69e79c988f6
Erwan DAYOT <pro.e.dayot@gmail.com>
2023-07-16 16:21:26 +0200

    chore: fix git hash

--- .github/workflows/artifact.py
+++ .github/workflows/artifact.py
@@ -3,15 +3,14 @@ import os
 
 
 #Get the git hash
-print(os.environ("git_hash"))
-
+git_hash = os.environ("GIT_HASH")
 
 
 #Replace all __git_hash__ with the git hash in ../../beet-artifact.yaml
 with open("../../beet-artifact.yaml", "r") as f:
     text = f.read()
 
-text = text.replace("__git_hash__", os.environ("git_hash"))
+text = text.replace("__git_hash__", git_hash)
 
 with open("../../beet-artifact.yaml", "w") as f:
     f.write(text)
