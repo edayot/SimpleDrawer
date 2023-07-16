b98d87d2b6ce9e574997423e55edb9946808d858
Erwan DAYOT <pro.e.dayot@gmail.com>
2023-07-16 16:36:39 +0200

    chore: aaa

--- .github/workflows/artifact.py
+++ .github/workflows/artifact.py
@@ -4,16 +4,16 @@ import os
 
 #Get the git hash
 git_hash = os.environ["GIT_HASH"]
-print(git_hash, os.listdir("../"))
+print(git_hash, os.listdir("."))
 
 
 #Replace all __git_hash__ with the git hash in ../../beet-artifact.yaml
-with open("../beet-artifact.yaml", "r") as f:
+with open("beet-artifact.yaml", "r") as f:
     text = f.read()
 
 text = text.replace("__git_hash__", git_hash)
 
-with open("../beet-artifact.yaml", "w") as f:
+with open("beet-artifact.yaml", "w") as f:
     f.write(text)
 
 
