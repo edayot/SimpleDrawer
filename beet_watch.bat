7f69b47cb80bf159cc73bf205ea2f8d9b2177122
edayot <pro.e.dayot@gmail.com>
2023-09-09 12:02:11 +0200

    chore: update dep

--- beet_watch.bat
+++ beet_watch.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
-py -m beet l --minecraft "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\1.20.1\.minecraft" --data-pack "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\1.20.1\.minecraft\resourcepacks"
+py -m beet l --minecraft "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\1.20.1\.minecraft" 
+py -m beet l --resource-pack "C:\Users\erwan\AppData\Roaming\PrismLauncher\instances\1.20.1\.minecraft\resourcepacks"
+py -m beet l --data-pack "C:\Users\erwan\Documents\Dev\Server\world\datapacks"
+
 py -m beet --project .\beet-dev.yaml w
 
 
