4b2ae2c335a3562a8ec8c2746efb7f5ce98c1bc0
edayot <pro.e.dayot@gmail.com>
2024-06-11 12:23:07 +0200

    feat: using the render for pack.png

---
+++ pack.py
@@ -0,0 +1,10 @@
+from beet import Context
+
+
+
+
+def beet_default(ctx: Context):
+    render_path = "simpledrawer:render/simpledrawer/block/new_drawer/oak_full_drawers_1"
+    tex = ctx.assets.textures[render_path]
+    ctx.data.extra["pack.png"] = tex
+    ctx.assets.extra["pack.png"] = tex
